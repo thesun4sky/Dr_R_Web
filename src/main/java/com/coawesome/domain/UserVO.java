5577f45ffbf755266b45175c6a89dabf07c2aac5
ghtpdkzz <ghtpdkzz@naver.com>
2016-11-08 16:46:46 +0900

    no lombok

--- src/main/java/com/coawesome/domain/UserVO.java
+++ src/main/java/com/coawesome/domain/UserVO.java
@@ -1,17 +1,37 @@
 package com.coawesome.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Created by TeasunKim on 2016-11-03.
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class UserVO {
     private int u_id;
     private String login_id;
     private String u_name;
+
+    public int getU_id() {
+        return u_id;
+    }
+
+    public String getLogin_id() {
+        return login_id;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setLogin_id(String login_id) {
+        this.login_id = login_id;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+
+    public void setU_id(int u_id) {
+        this.u_id = u_id;
+    }
 }
