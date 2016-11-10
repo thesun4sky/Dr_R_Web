9ba966c3b63392f883d4c69d48e6f42b59bb29a6
ghtpdkzz <ghtpdkzz@naver.com>
2016-11-10 22:58:18 +0900

    Join API 구현

--- src/main/java/com/coawesome/domain/UserVO.java
+++ src/main/java/com/coawesome/domain/UserVO.java
@@ -13,43 +13,9 @@ import lombok.NoArgsConstructor;
 public class UserVO {
     private int u_id;
     private String login_id;
-    private String u_password;
     private String u_name;
-    private String u_hospital;
-
-    public int getU_id() {
-        return u_id;
-    }
-
-    public String getLogin_id() {
-        return login_id;
-    }
-
-    public String getU_name() {
-        return u_name;
-    }
-
-    public  String getU_password(){
-        return u_password;
-    }
-    public  String getU_hospital(){
-        return u_hospital;
-    }
-    public void setLogin_id(String login_id) {
-        this.login_id = login_id;
-    }
-
-    public void setU_name(String u_name) {
-        this.u_name = u_name;
-    }
-
-    public void setU_id(int u_id) {
-        this.u_id = u_id;
-    }
-    public void setU_password(String u_password){
-        this.u_password = u_password;
-    }
-    public void setU_hospital(String u_hospital){
-        this.u_hospital = u_hospital;
-    }
+    private String phone;
+    private String password;
+    private String disease;
+    private String hospital_name;
 }
