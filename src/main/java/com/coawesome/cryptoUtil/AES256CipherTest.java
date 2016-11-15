175e71a577e7062ef9ba598259f0526414af7136
thesun4sky <thesun4sky@gmail.com>
2016-11-15 22:27:43 +0900

    사이퍼 테스트 버그수정

--- src/main/java/com/coawesome/cryptoUtil/AES256CipherTest.java
+++ src/main/java/com/coawesome/cryptoUtil/AES256CipherTest.java
@@ -3,7 +3,6 @@ package com.coawesome.cryptoUtil;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import org.junit.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -18,12 +17,12 @@ public class AES256CipherTest {
     {
         this.password =pass;
     }
-    @Test public String encPass() throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+     public String encPass() throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
         AES256Cipher a256 = AES256Cipher.getInstance();
         String enPassword = a256.AES_Encode(password);
         return enPassword;
     }
-    @Test public String desPass() throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
+     public String desPass() throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
         AES256Cipher a256 = AES256Cipher.getInstance();
         String desPassword = a256.AES_Decode(password);
         return desPassword;
