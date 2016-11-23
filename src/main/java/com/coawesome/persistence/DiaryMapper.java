cb695c9b88f56491cc6609369b0a8f3b4a273218
mslee1002 <leemsmail@naver.com>
2016-11-24 02:32:14 +0900

    일지 작성 API수정

--- src/main/java/com/coawesome/persistence/DiaryMapper.java
+++ src/main/java/com/coawesome/persistence/DiaryMapper.java
@@ -21,8 +21,8 @@ public interface DiaryMapper {
     DiaryVO getDiary(DiaryVO diaryVO);
 
     //다이어리 쓰기
-    @Insert("INSERT INTO check_list(u_id, c_temperature, c_humid, c_sleepTime, c_drinking, c_bloodPressure, c_memo, c_breakfast, c_lunch, c_dinner)" +
-            " VALUES(#{u_id}, #{c_temperature}, #{c_humid}, #{c_sleepTime}, #{c_drinking}, #{c_bloodPressure}, #{c_memo}, #{c_breakfast}, #{c_lunch}, #{c_dinner})")
+    @Insert("INSERT INTO check_list(u_id, c_temperature, c_humid, c_sleepTime, c_drinking, c_bloodPressure,  c_breakfast, c_lunch, c_dinner)" +
+            " VALUES(#{u_id}, #{c_temperature}, #{c_humid}, #{c_sleepTime}, #{c_drinking}, #{c_bloodPressure},  #{c_breakfast}, #{c_lunch}, #{c_dinner})")
     void addDiary(DiaryVO diaryVO);
 
 
