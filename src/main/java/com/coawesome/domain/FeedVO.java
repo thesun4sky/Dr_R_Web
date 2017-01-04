0fdcaead5f08dde0ad4bbabe0eca85b586e10c8f
thesun4sky <thesun4sky@gmail.com>
2017-01-05 02:50:06 +0900

    test added(2)

--- src/main/java/com/coawesome/domain/FeedVO.java
+++ src/main/java/com/coawesome/domain/FeedVO.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FeedVO {
-    private int f_id;
-    private int u_id;
-    private String u_name;
-    private java.sql.Timestamp f_start;
-    private java.sql.Timestamp f_end;
-    private int f_total;
-    private String feed;
+    private int f_id=0;
+    private int u_id=0;
+    private String u_name="";
+    private java.sql.Timestamp f_start=new Timestamp(11111111);
+    private java.sql.Timestamp f_end=new Timestamp(11111111);
+    private int f_total = 0;
+    private String feed = "";
 }
--- src/main/java/com/coawesome/domain/SleepVO.java
+++ src/main/java/com/coawesome/domain/SleepVO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 
 /**
  * Created by TeasunKim on 2016-11-03.
@@ -12,10 +14,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SleepVO {
-    private int s_id;
-    private int u_id;
-    private String u_name;
-    private java.sql.Timestamp s_start;
-    private java.sql.Timestamp s_end;
-    private int s_total;
+    private int s_id=0;
+    private int u_id=0;
+    private String u_name="";
+    private java.sql.Timestamp s_start=new Timestamp(11111111);
+    private java.sql.Timestamp s_end=new Timestamp(11111111);
+    private int s_total=0;
 }
